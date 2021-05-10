50eeb3a28b7257c8404f8e9c8aa4ce15ccfa291b
SantiMinio <santiago.mino@davinci.edu.ar>
2021-05-10 17:09:49 -0300

    Yo vs Canvas: Preludio

---
+++ Assets/Scripts/Cards/DeckOfCards.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckOfCards : MonoBehaviour
+{
+    [SerializeField] Card[] deck = new Card[9];
+    [SerializeField] int numberOfMaxCards = 5;
+
+    List<Card> currentDeck = new List<Card>();
+    List<Card> currentCards = new List<Card>();
+
+    [SerializeField] RectTransform firstPos = null;
+    [SerializeField] RectTransform lastPos = null;
+    [SerializeField] RectTransform posToSpawn = null;
+
+    Vector3[] positions = new Vector3[0];
+
+    private void Start()
+    {
+        positions = new Vector3[numberOfMaxCards];
+        float spacingBetweenCards = (firstPos.position.x - lastPos.position.x) / (numberOfMaxCards -1);
+        Vector3 currentPos = firstPos.position;
+        for (int i = 0; i < numberOfMaxCards; i++)
+        {
+            positions[i] = currentPos;
+            currentPos.x -= spacingBetweenCards;
+        }
+
+        SpawnCards();
+
+        for (int i = 0; i < numberOfMaxCards; i++)
+        {
+            SelectRandomCard();
+        }
+    }
+
+    void SpawnCards()
+    {
+        for (int i = 0; i < deck.Length; i++)
+        {
+            var card = Instantiate(deck[i], transform).GetComponent<Card>();
+            currentDeck.Add(card);
+            card.gameObject.SetActive(false);
+        }
+    }
+
+    void SelectRandomCard()
+    {
+        Card randomCard = currentDeck[Random.Range(0, currentDeck.Count)];
+        currentDeck.Remove(randomCard);
+        randomCard.gameObject.SetActive(true);
+        currentCards.Add(randomCard);
+        randomCard.transform.position = positions[currentCards.Count - 1];
+    }
+}
