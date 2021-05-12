0f4c31724355d4d17d40645607cc7f7a64313ede
Gonzalou <56851977+Gonzalou@users.noreply.github.com>
2021-05-12 04:50:47 -0300

    retoques y un script que destruye lo que se cae de la mesa

--- Assets/FalopaFran/Scripts/Destroyerground.cs
+++ Assets/FalopaFran/Scripts/Destroyerground.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Destroyerground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(collision.gameObject);
+       
     }
 }
