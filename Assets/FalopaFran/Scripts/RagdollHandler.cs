b305d9e9a7516b60d47b64b1b3c365c6d254799f
Frano <frano.criscuolo@gmail.com>
2021-05-15 19:32:24 -0300

    Adios rigidbodies

--- Assets/FalopaFran/Scripts/RagdollHandler.cs
+++ Assets/FalopaFran/Scripts/RagdollHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,36 @@ public class RagdollHandler : MonoBehaviour
     [SerializeField] private Rigidbody hip;
     
     [SerializeField] private float pushbackForce;
-    
+
+
+    private float _count;
+    private bool countDown;
+    [SerializeField] private float countDownToDestroy;
     void Awake()
     {
         _myRb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (countDown)
+        {
+            _count += Time.deltaTime;
+            if (_count >= countDownToDestroy)
+            {
+                Destroy(gameObject);
+            }    
+        }
+        
+        
+        
+    }
+
     public void PushTo(Vector3 lastDirAttack)
     {
-        Debug.Log(lastDirAttack);
-        
-        
+        //Debug.Log(lastDirAttack);
+
+        countDown = true;
         hip.AddForce(pushbackForce * lastDirAttack, ForceMode.Impulse);   
     }
 }
